f06574bae4623a71bded01b80df62e144b6f4af0
HaruoRafael <rafael.haruo.nh@outlook.com>
2024-10-17 15:57:25 -0300

    17/10/2024

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -16,7 +16,7 @@
 
         <!-- Password -->
         <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" class="text-yellow-500" />
+            <x-input-label for="password" :value="__('Senha')" class="text-yellow-500" />
             <input id="password" 
                    class="block mt-1 w-full bg-[#1a1a1a] text-yellow-500 border-gray-700 focus:border-yellow-500 focus:ring-yellow-500 rounded-md shadow-sm"
                    type="password" name="password" required autocomplete="current-password" style="background-color: #1a1a1a; color: #fcd34d;" />
@@ -27,19 +27,15 @@
         <div class="block mt-4">
             <label for="remember_me" class="inline-flex items-center">
                 <input id="remember_me" type="checkbox" class="rounded dark:bg-gray-900 border-gray-300 dark:border-gray-700 text-yellow-500 shadow-sm focus:ring-yellow-500" name="remember">
-                <span class="ml-2 text-sm text-yellow-500">{{ __('Remember me') }}</span>
+                <span class="ml-2 text-sm text-yellow-500">{{ __('Manter-me Logado') }}</span>
             </label>
         </div>
 
         <div class="flex items-center justify-between mt-4">
-            @if (Route::has('password.request'))
-                <a class="underline text-sm text-yellow-500 hover:text-yellow-400" href="{{ route('password.request') }}">
-                    {{ __('Forgot your password?') }}
-                </a>
-            @endif
+          
 
             <x-primary-button class="ml-3 bg-yellow-500 hover:bg-yellow-600 text-black font-bold py-2 px-4 rounded">
-                {{ __('Log in') }}
+                {{ __('Logar') }}
             </x-primary-button>
         </div>
     </form>
